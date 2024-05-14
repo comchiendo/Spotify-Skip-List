337204e38d47990baa0f148f0e3d173d6e9f3d67
Anthony Do <anthonydo02@yahoo.com>
2024-05-14 10:37:11 -0700

    Counted number of skips for each song (only ones flagged by SPotify).

--- main.py
+++ main.py
@@ -6,6 +6,15 @@ f = open('Streaming_History_Audio_2022-2024_1.json')
 
 data = json.load(f)
 
+skip_count = {}
 for i in data:
     if i["skipped"] == True:
         print(i["master_metadata_track_name"])
+        
+        if i["master_metadata_track_name"] in skip_count:
+            skip_count[i["master_metadata_track_name"]] += 1
+        else:
+            skip_count[i["master_metadata_track_name"]] = 1
+
+#print(skip_count)
+print(skip_count.values())
